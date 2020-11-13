3fb8b5070c00bf2a40b90941e7a567ae7fb935ab
wanggui <283495283@qq.com>
2020-11-13 11:29:07 +0800

    插件中使用宿主中公用loading

--- app_sub1/src/main/java/com/app/sub/activity/MainSub4Activity.java
+++ app_sub1/src/main/java/com/app/sub/activity/MainSub4Activity.java
@@ -3,13 +3,35 @@ package com.app.sub.activity;
 import android.os.Bundle;
 
 import com.app.sub.R;
+import com.common.business.code.activity.BasePresenter;
+import com.common.business.code.activity.BaseViewModelActivity;
 import com.common.framework.activity.PluginCommonActivity;
 
-public class MainSub4Activity extends PluginCommonActivity {
+public class MainSub4Activity extends BaseViewModelActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_sub4);
+    }
+
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected Class getModelClass() {
+        return null;
+    }
+
+    @Override
+    protected void initControl(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initValue() {
+
     }
 }
