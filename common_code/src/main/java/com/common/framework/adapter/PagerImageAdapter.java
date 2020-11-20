82c09b9f4a22f43c6b7fbbe79986e7054348d4c7
wanggui <283495283@qq.com>
2020-11-20 17:23:52 +0800

    .

---
+++ common_code/src/main/java/com/common/framework/adapter/PagerImageAdapter.java
@@ -0,0 +1,37 @@
+package com.common.framework.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+/**
+ * @author：atar
+ * @date: 2020/11/19
+ * @description:
+ */
+public class PagerImageAdapter<T> extends PagerAdapter {
+    protected List<T> list;
+
+    public PagerImageAdapter(List<T> list) {
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list != null ? list.size() : 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+}
